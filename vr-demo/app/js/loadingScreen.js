154547bd772a479ee492b0b3e63d57a9a2c80d5f
Kevin Alwell <alwellkevin44@gmail.com>
2018-01-29 18:31:03 -0500

    Include Core Files

---
+++ vr-demo/app/js/loadingScreen.js
@@ -0,0 +1,7 @@
+/*!
+    loading script
+*/
+document.querySelector(".enter-vr").addEventListener('click', function(){
+  document.querySelector("#background-landing").remove();
+  document.querySelector("a-scene").style.display = "block";
+})
