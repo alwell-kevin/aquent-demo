32d4d3a1b33845fe19f05b4f9d247bc4c4f3869c
Kevin Alwell <alwellkevin44@gmail.com>
2018-01-29 20:20:43 -0500

    Finish Refactor remove unwanted libraries

--- vr-demo/app/js/loadingScreen.js
+++
@@ -1,7 +0,0 @@
-/*!
-    loading script
-*/
-document.querySelector(".enter-vr").addEventListener('click', function(){
-  document.querySelector("#background-landing").remove();
-  document.querySelector("a-scene").style.display = "block";
-})
