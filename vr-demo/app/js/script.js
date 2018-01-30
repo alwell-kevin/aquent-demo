2d50e0c3da9a4b543b6e778169b8903e2fb18d01
Kevin Alwell <alwellkevin44@gmail.com>
2018-01-29 19:26:42 -0500

    Refactor to arrow functions, simplify getting url param

--- vr-demo/app/js/script.js
+++ vr-demo/app/js/script.js
@@ -1,54 +1,57 @@
 var TIMESTAMP = null;
 var LOCATION = 'entrance'; //starting position
 
-function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));        
+//Return 'Location' URL Parameter
+const getParameterByName = (name) => {
+    var url = new URL(document.location.href);
+    return url.searchParams.get(name);
 }
 
-// checking if there is a parameter in the URL / add one if not
-if ( getParameterByName("location") ) {
+//Checking if there is a 'Location' parameter in the URL. Add one if not.
+const onLoad = () => {
+    if (getParameterByName("location")) {
 
-    console.log('starting location set to: ' + getParameterByName("location"));
-    setTimeout(function () {
+        console.log('starting location set to: ' + getParameterByName("location"));
+        setTimeout(() => {
 
-        var targetEl = document.querySelector('a-sky');
-        var newLocation = getParameterByName("location");
-        var newLocationID = "#"+newLocation;
-        var image = document.getElementById(newLocation);
-        var offsetY = image.getAttribute("offset-y");
+            var targetEl = document.querySelector('a-sky');
+            var newLocation = getParameterByName("location");
+            var newLocationID = "#" + newLocation;
+            var image = document.getElementById(newLocation);
+            var offsetY = image.getAttribute("offset-y");
 
-        navigate(newLocationID);
+            navigate(newLocationID);
 
-        targetEl.setAttribute('material', 'src', newLocationID);
-        targetEl.setAttribute('rotation', 'y', offsetY);
-        targetEl.emit('fade-image-in');
-    }, 300);
+            targetEl.setAttribute('material', 'src', newLocationID);
+            targetEl.setAttribute('rotation', 'y', offsetY);
+            targetEl.emit('fade-image-in');
+        }, 300);
 
-} else {
-    
-    queryString.push('location', LOCATION); // query-string library
-    console.log('starting location init: ' + getParameterByName("location"));
-
-}
-
-// navigate to new location and fade out old location out / assume entrance if no old location
-function navigate(loc) {
-
-    var skyObj = document.querySelector('a-sky').components.material;
-    var oldNav = null;
-    
-    if ( typeof skyObj === 'undefined' ) {
-        oldNav = "nav_entrance"
     } else {
-        oldNav = "nav_" + skyObj.oldData.src.id;
+
+        queryString.push('location', LOCATION); // query-string library
+        console.log('starting location init: ' + getParameterByName("location"));
+
     }
-    
-    console.log("oldNav :"+ oldNav);
+}
+
+//Change Sky Image.
+//Set Markers related to the 'Location'.
+const navigate = (loc) => {
+    var oldNav,
+        skyObj = document.querySelector('a-sky').components.material;
+
+    if (skyObj) {
+        oldNav = "nav_" + skyObj.oldData.src.id;
+    } else {
+        oldNav = "nav_entrance"
+    }
+
+    console.log("oldNav :" + oldNav);
     document.getElementById(oldNav).setAttribute('visible', 'false');
     document.getElementById(oldNav).setAttribute('scale', '0.1 0.1 0.1');
     var nav = loc.replace("#", "nav_");
-    console.log("nav :"+ nav);
+    console.log("nav :" + nav);
     document.getElementById(nav).setAttribute('visible', 'true');
     document.getElementById(nav).setAttribute('scale', '1 1 1');
 }
@@ -56,12 +59,30 @@ function navigate(loc) {
 // Clickable marker component
 AFRAME.registerComponent('marker', {
     schema: {
-        icon: {type: 'string', default: 'icn_point'},
-        enterColor: {type: 'string', default: '#ffffff'},
-        leaveColor: {type: 'string', default: '#ffffff'},
-        yRot: {type: 'number', default: 0},
-        yPos: {type: 'number', default: 0},
-        zPos: {type: 'number', default: -3}
+        icon: {
+            type: 'string',
+            default: 'icn_point'
+        },
+        enterColor: {
+            type: 'string',
+            default: '#ffffff'
+        },
+        leaveColor: {
+            type: 'string',
+            default: '#ffffff'
+        },
+        yRot: {
+            type: 'number',
+            default: 0
+        },
+        yPos: {
+            type: 'number',
+            default: 0
+        },
+        zPos: {
+            type: 'number',
+            default: -3
+        }
     },
     init: function () {
         var data = this.data;
@@ -75,16 +96,16 @@ AFRAME.registerComponent('marker', {
         marker.setAttribute('material', `shader: flat; transparent:true; opacity: 0.75; color: ${data.leaveColor}; src: #${data.icon}`);
         marker.setAttribute('position', `0 0 ${data.zPos}`);
         marker.setAttribute('rotation', '0 0 0');
-        marker.setAttribute('scale', '1 1 1');    
+        marker.setAttribute('scale', '1 1 1');
         el.appendChild(marker);
 
-        marker.addEventListener('mouseenter', function(evt) {
-            this.setAttribute('material', `color: ${data.enterColor}; opacity: 0.95;`);  
-            this.setAttribute('scale', '1.25 1.25 1.25');  
+        marker.addEventListener('mouseenter', function (evt) {
+            this.setAttribute('material', `color: ${data.enterColor}; opacity: 0.95;`);
+            this.setAttribute('scale', '1.25 1.25 1.25');
         });
-        marker.addEventListener('mouseleave', function(evt) {
-            this.setAttribute('material', `color: ${data.leaveColor}; opacity: 0.75;`);  
-            this.setAttribute('scale', '1 1 1');  
+        marker.addEventListener('mouseleave', function (evt) {
+            this.setAttribute('material', `color: ${data.leaveColor}; opacity: 0.75;`);
+            this.setAttribute('scale', '1 1 1');
         });
     }
 });
@@ -92,10 +113,21 @@ AFRAME.registerComponent('marker', {
 // Component to change sky on click and set markers/hotspots.
 AFRAME.registerComponent('enter-room', {
     schema: {
-        on: {type: 'string', default: 'click'},
-        target: {type: 'selector', default: 'a-sky'},
-        src: {type: 'string'},
-        dur: {type: 'number', default: 300}
+        on: {
+            type: 'string',
+            default: 'click'
+        },
+        target: {
+            type: 'selector',
+            default: 'a-sky'
+        },
+        src: {
+            type: 'string'
+        },
+        dur: {
+            type: 'number',
+            default: 300
+        }
     },
 
     init: function () {
@@ -107,7 +139,7 @@ AFRAME.registerComponent('enter-room', {
 
         el.addEventListener(data.on, function (evt) {
 
-            if(TIMESTAMP == null || TIMESTAMP + 1000 < evt.timeStamp) {
+            if (TIMESTAMP == null || TIMESTAMP + 1000 < evt.timeStamp) {
 
                 var imageID = data.src.replace("#", "");
                 var image = document.getElementById(imageID);
@@ -117,7 +149,7 @@ AFRAME.registerComponent('enter-room', {
                 targetEl.emit('fade-image-out');
 
                 // Wait for fade to complete.
-                setTimeout(function () {
+                setTimeout(() => {
                     // Set image.
                     navigate(data.src);
                     queryString.push('location', data.src.replace("#", ""));
@@ -132,13 +164,15 @@ AFRAME.registerComponent('enter-room', {
         });
     },
     /**
-    * Setup fade-in + fade-out.
-    */
+     * Setup fade-in + fade-out.
+     */
     setupFadeAnimation: function () {
         var data = this.data;
         var targetEl = this.data.target;
         // Only set up once.
-        if (targetEl.dataset.setImageFadeSetup) { return; }
+        if (targetEl.dataset.setImageFadeSetup) {
+            return;
+        }
         targetEl.dataset.setImageFadeSetup = true;
 
         // Create animations.
@@ -168,19 +202,22 @@ AFRAME.registerComponent('rotate-with-camera', {
     },
     tick: function () {
         var rotation = this.cameraEl.object3D.getWorldRotation();
-        this.el.setAttribute("rotation", "-90 0 " + Math.round(THREE.Math.radToDeg(rotation.y) * 100) / 100 );
+        this.el.setAttribute("rotation", "-90 0 " + Math.round(THREE.Math.radToDeg(rotation.y) * 100) / 100);
     }
 });
 
 // component linking to URL
 AFRAME.registerComponent('outlink', {
     schema: {
-        url: {type: 'string', default: ''},
+        url: {
+            type: 'string',
+            default: ''
+        },
     },
     init: function () {
         var data = this.data;
-        this.el.addEventListener('click', function(evt) {
-            window.open(data.url,"_self")
+        this.el.addEventListener('click', function (evt) {
+            window.open(data.url, "_self")
         });
     }
 });
@@ -205,9 +242,11 @@ AFRAME.registerComponent('yhelper', {
         this.el.appendChild(yDisplay);
         this.yDisplay = yDisplay;
     },
-    tick: function () {   
+    tick: function () {
         var rotation = this.el.object3D.getWorldRotation();
-        this.yDisplay.setAttribute('text', 'value', Math.round(THREE.Math.radToDeg(rotation.y) * 100) / 100 );
-    }    
+        this.yDisplay.setAttribute('text', 'value', Math.round(THREE.Math.radToDeg(rotation.y) * 100) / 100);
+    }
 });
 
+//Loads Initial Scene.
+onLoad();
